10fbfcad73e32c59264f072882c29a52c79c48e5
Jason <atwill007@163.com>
2020-04-25 23:57:49 +0800

    feat: 换一种写法，有没有感觉和React的Hook惊人的相似😂

--- src/views/CompositionAPI.vue
+++ src/views/CompositionAPI.vue
@@ -7,21 +7,26 @@
 <script>
 import { reactive, computed } from "vue";
 
+// 这和React的Hook也太像了吧 😂
+function useCount(defaultVal) {
+  const state = reactive({
+    count: defaultVal,
+    double: computed(() => state.count * 2)
+  });
+
+  function increment() {
+    state.count++;
+  }
+
+  return {
+    state,
+    increment
+  };
+}
+
 export default {
   setup() {
-    const state = reactive({
-      count: 0,
-      double: computed(() => state.count * 2)
-    });
-
-    function increment() {
-      state.count++;
-    }
-
-    return {
-      state,
-      increment
-    };
+    return useCount(1);
   }
 };
 </script>
